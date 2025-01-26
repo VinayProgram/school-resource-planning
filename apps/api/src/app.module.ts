1b4b272ae9ac0100fd1310674fb8a51c4cfb49c7
VinayProgram <vinaytandale@gmail.com>
2025-01-26 09:31:18 +0000

    graphql implemention

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,9 +2,25 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DrizzleModule } from './drizzle/drizzle.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver } from '@nestjs/apollo';
+import { join } from 'path';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [DrizzleModule],
+  imports: [
+    GraphQLModule.forRoot({
+      driver:ApolloDriver,
+      introspection:true,
+      fieldResolverEnhancers:['guards'],
+      autoSchemaFile:join(process.cwd(),'src/schema.gql'),
+      buildSchemaOptions:{
+        numberScalarMode:'integer'
+      }
+    }),
+    DrizzleModule,
+    UsersModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
